4e7080ae40c6d2d9f6d760dba0daad63fa454a29
Tina Giammarella <tgiammarella@gmail.com>
2026-05-30 17:02:29 -0400

    Add rewrites for atelier-rosemere path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/atelier-rosemere",
+        destination: "https://synvista-rosemere-29juin.vercel.app/",
+      },
+      {
+        source: "/atelier-rosemere/:path*",
+        destination: "https://synvista-rosemere-29juin.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
